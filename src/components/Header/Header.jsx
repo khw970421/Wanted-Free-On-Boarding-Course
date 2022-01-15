1847d6c5e9566db1a023472f96f57e3931521617
khw970421 <khw970421@kakao.com>
2022-01-15 17:19:47 +0900

    🐛  fix : 반응형 중첩 부분 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,31 @@
-import React from "react"
-import HeaderPc from './HeaderPc'
-import HeaderLTablet from './HeaderLTablet'
-import HeaderSTablet from './HeaderSTablet'
-import HeaderMobile from './HeaderMobile'
-import { useMediaQuery } from "react-responsive"
-const Header = ()=>{
-    const isPc = useMediaQuery({
-        query : "(min-width:1200px)"
-      });
-      const isLargeTablet = useMediaQuery({
-        query : "(min-width:990px) and (max-width:1200px)"
-      });
-      const isSmallTablet = useMediaQuery({
-        query : "(min-width:766px) and (max-width:990px)"
-      });
-      const isMobile = useMediaQuery({
-        query : "(max-width:766px)"
-      });
-      
-    
-      return (
-      <div>
-        {isPc && <HeaderPc/>}
-        {isLargeTablet && <HeaderLTablet/>}
-        {isSmallTablet && <HeaderSTablet/>}
-        {isMobile && <HeaderMobile/>}
-      </div>
-      )
-}
+import React from "react";
+import HeaderPc from "./HeaderPc";
+import HeaderLTablet from "./HeaderLTablet";
+import HeaderSTablet from "./HeaderSTablet";
+import HeaderMobile from "./HeaderMobile";
+import { useMediaQuery } from "react-responsive";
+const Header = () => {
+  const isPc = useMediaQuery({
+    query: "(min-width:1200px)",
+  });
+  const isLargeTablet = useMediaQuery({
+    query: "(min-width:990px) and (max-width:1199px)",
+  });
+  const isSmallTablet = useMediaQuery({
+    query: "(min-width:766px) and (max-width:989px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:765px)",
+  });
 
-export default Header
+  return (
+    <div>
+      {isPc && <HeaderPc />}
+      {isLargeTablet && <HeaderLTablet />}
+      {isSmallTablet && <HeaderSTablet />}
+      {isMobile && <HeaderMobile />}
+    </div>
+  );
+};
+
+export default Header;
