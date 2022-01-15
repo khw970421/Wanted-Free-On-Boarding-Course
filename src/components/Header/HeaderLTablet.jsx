5f6ab419c8dd5a7cd36aeeca615857cde323dc32
khw970421 <khw970421@kakao.com>
2022-01-15 20:26:29 +0900

    ♻️ refactor : 가독성을 위해 Icon 컴포넌트 분리

--- src/components/Header/HeaderLTablet.jsx
+++ src/components/Header/HeaderLTablet.jsx
@@ -1,5 +1,8 @@
 import menuIcon from "../../imgs/icon/icon-menu.png";
 import { headerContentArr } from "../../utils/constant";
+
+import SearchIcon from "../HeaderIcon/SearchIcon";
+
 import {
   headerMainStyle,
   headerContentStyle,
@@ -9,7 +12,6 @@ import {
   headerLTabletStyle,
   headerContainerStyle,
 } from "./css.js";
-
 
 const HeaderLTablet = () => {
   return (
@@ -27,21 +29,7 @@ const HeaderLTablet = () => {
           ))}
         </div>
         <div class="rightHeader" style={headerSubStyle}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-search"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-          </svg>
+          <SearchIcon />
           <div>회원가입/로그인</div>
           <div>|</div>
           <button style={headerGovServiceButtonStyle}>기업 서비스</button>
--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -1,5 +1,8 @@
 import menuIcon from "../../imgs/icon/icon-menu.png";
 import {headerMobileContentArr } from "../../utils/constant";
+import SearchIcon from "../HeaderIcon/SearchIcon";
+import MoreHorizontalIcon from "../HeaderIcon/MoreHorizontalIcon";
+
 import {
     headerMainStyle,
     iconStyle,
@@ -33,53 +36,8 @@ const HeaderMobile = () => {
             ))}
           </div>
           <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-search"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-more-horizontal"
-            >
-              <circle cx="12" cy="12" r="1" />
-              <circle cx="19" cy="12" r="1" />
-              <circle cx="5" cy="12" r="1" />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-more-horizontal"
-              >
-                <circle cx="12" cy="12" r="1" />
-                <circle cx="19" cy="12" r="1" />
-                <circle cx="5" cy="12" r="1" />
-              </svg>
-            </svg>
+            <SearchIcon/>
+            <MoreHorizontalIcon/>
           </div>
         </div>
       </div>
