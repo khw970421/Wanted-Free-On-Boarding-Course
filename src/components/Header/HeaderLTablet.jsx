281d19f59369d3fd6a820ba29598aff4841c4e5a
khw970421 <khw970421@kakao.com>
2022-01-15 18:10:45 +0900

    🎨 chore : CSS 코드 분리

--- src/components/Header/HeaderLTablet.jsx
+++ src/components/Header/HeaderLTablet.jsx
@@ -1,11 +1,13 @@
 import menuIcon from "../../imgs/icon/icon-menu.png";
 import {
-    headerMainStyle,
-    headerContentStyle,
-    iconStyle,
-    headerSubStyle,
-    headerGovServiceButtonStyle,
-  } from "./css.js";
+  headerMainStyle,
+  headerContentStyle,
+  iconStyle,
+  headerSubStyle,
+  headerGovServiceButtonStyle,
+  headerLTabletStyle,
+  headerContainerStyle,
+} from "./css.js";
 const headerContentArr = [
   "채용",
   "이벤트",
@@ -18,56 +20,42 @@ const headerContentArr = [
 
 const HeaderLTablet = () => {
   return (
-    <div style={headerStyle}>
-      <div class="leftHeader" style={headerMainStyle}>
-        <div>
-          <img src={menuIcon} style={iconStyle} />
+    <section class="Header" style={headerContainerStyle}>
+      <div style={headerLTabletStyle}>
+        <div class="leftHeader" style={headerMainStyle}>
+          <div>
+            <img src={menuIcon} style={iconStyle} />
+          </div>
+          <div>wanted</div>
         </div>
-        <div>wanted</div>
+        <div class="centerHeader" style={headerContentStyle}>
+          {headerContentArr.map((headerContent) => (
+            <div>{headerContent}</div>
+          ))}
+        </div>
+        <div class="rightHeader" style={headerSubStyle}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-search"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+          </svg>
+          <div>회원가입/로그인</div>
+          <div>|</div>
+          <button style={headerGovServiceButtonStyle}>기업 서비스</button>
+        </div>
       </div>
-      <div class="centerHeader" style={headerContentStyle}>
-        {headerContentArr.map((headerContent) => (
-          <div>{headerContent}</div>
-        ))}
-      </div>
-      <div class="rightHeader" style={headerSubStyle}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-search"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-        </svg>
-        <div>회원가입/로그인</div>
-        <div>|</div>
-        <button style={headerButton}>기업 서비스</button>
-      </div>
-    </div>
+    </section>
   );
-};
-
-const headerStyle = {
-  display: "flex",
-  width: "100%",
-  fontWeight: "bolder",
-  justifyContent: "space-evenly",
-  alignItems: "center",
-  height: "50px",
-};
-
-const headerButton = {
-  border: "1px solid gray",
-  borderRadius: "12px",
-  backgroundColor: "white",
-  color: "gray",
 };
 
 export default HeaderLTablet;
--- src/components/Header/HeaderPc.jsx
+++ src/components/Header/HeaderPc.jsx
@@ -5,6 +5,8 @@ import {
   iconStyle,
   headerSubStyle,
   headerGovServiceButtonStyle,
+  headerPcStyle,
+  headerContainerStyle,
 } from "./css.js";
 const headerContentArr = [
   "채용",
@@ -18,49 +20,42 @@ const headerContentArr = [
 
 const HeaderPc = () => {
   return (
-    <div style={headerStyle}>
-      <div class="leftHeader" style={headerMainStyle}>
-        <div>
-          <img src={menuIcon} style={iconStyle} />
+    <section class="Header" style={headerContainerStyle}>
+      <div style={headerPcStyle}>
+        <div class="leftHeader" style={headerMainStyle}>
+          <div>
+            <img src={menuIcon} style={iconStyle} />
+          </div>
+          <div>wanted</div>
         </div>
-        <div>wanted</div>
+        <div class="centerHeader" style={headerContentStyle}>
+          {headerContentArr.map((headerContent) => (
+            <div>{headerContent}</div>
+          ))}
+        </div>
+        <div class="rightHeader" style={headerSubStyle}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-search"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+          </svg>
+          <div>회원가입/로그인</div>
+          <div>|</div>
+          <button style={headerGovServiceButtonStyle}>기업 서비스</button>
+        </div>
       </div>
-      <div class="centerHeader" style={headerContentStyle}>
-        {headerContentArr.map((headerContent) => (
-          <div>{headerContent}</div>
-        ))}
-      </div>
-      <div class="rightHeader" style={headerSubStyle}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-search"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-        </svg>
-        <div>회원가입/로그인</div>
-        <div>|</div>
-        <button style={headerGovServiceButtonStyle}>기업 서비스</button>
-      </div>
-    </div>
+    </section>
   );
-};
-
-const headerStyle = {
-  display: "flex",
-  width: "100%",
-  fontWeight: "bolder",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "50px",
 };
 
 export default HeaderPc;
