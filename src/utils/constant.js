c1e50eaa066c72734d099a3f8c324305538a8f94
khw970421 <khw970421@kakao.com>
2022-01-16 15:10:56 +0900

    🎨 chore: slider 관련 데이터 추가

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,13 @@
+import slider1 from "../imgs/slider/slider1.jpg"
+import slider2 from "../imgs/slider/slider2.jpg"
+import slider3 from "../imgs/slider/slider3.jpg"
+import slider4 from "../imgs/slider/slider4.jpg"
+import slider5 from "../imgs/slider/slider5.jpg"
+import slider6 from "../imgs/slider/slider6.jpg"
+import slider7 from "../imgs/slider/slider7.jpg"
+import slider8 from "../imgs/slider/slider8.jpg"
+import slider9 from "../imgs/slider/slider9.jpg"
+
 const headerContentArr = [
   {
     contentName: "채용",
@@ -26,4 +36,52 @@ const headerContentArr = [
 
 const headerMobileContentArr = ["홈", "채용", "이벤트"];
 
-export { headerContentArr, headerMobileContentArr };
+const sliderDataArr = [
+  {
+    idx : slider1,
+    mainText : "개발자 되고싶은 분들?!",
+    subText : "프론트엔드 무료 교육과정 참여하기"
+  },
+  {
+    idx : slider2,
+    mainText : "마케팅 주니어를 찾습니다.",
+    subText : "1월 17일(월) 설명회 신청하기"
+  },
+  {
+    idx : slider3,
+    mainText : "성과를 내는 마케팅",
+    subText : "실제사례를 공개합니다!"
+  },
+  {
+    idx : slider4,
+    mainText : "포트폴리오를 부탁해!",
+    subText : "디자이너의 포트폴리오 살펴보기"
+  },
+  {
+    idx : slider5,
+    mainText : "UX 디자이너의 커리어 설계",
+    subText : "브랜드 가치를 더하는 디자인"
+  },
+  {
+    idx : slider6,
+    mainText : "개발자 성장 비결 공개",
+    subText : "Velog, 글쓰는 개발자들의 이야기"
+  },
+  {
+    idx : slider7,
+    mainText : "2022년 달라지는 노동법령",
+    subText : "노무관리 쟁점 한 눈에 파악하기"
+  },
+  {
+    idx : slider8,
+    mainText : "해, 커리어 EP 02 공개",
+    subText : "마지막 관문 2라운드의 승자는?"
+  },
+  {
+    idx : slider9,
+    mainText : "성장하는 개발자가 되려면?",
+    subText : "OOO 검색하지 말 것!"
+  },
+]
+
+export { headerContentArr, headerMobileContentArr ,sliderDataArr };
