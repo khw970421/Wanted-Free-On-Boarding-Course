53f2a60a7c33e856c08906ac64704b21d97837d2
khw970421 <khw970421@kakao.com>
2022-01-16 15:10:40 +0900

    💄 style : Slider 관련 CSS 코드 추가

---
+++ src/components/Slider/css.js
@@ -0,0 +1,31 @@
+const SliderExceptPcMainTextStyle = {
+  marginTop: "20px",
+  fontSize: "18px",
+  lineHeight: 1,
+  fontWeight: 700,
+};
+
+const SliderExceptPcSubTextStyle = {
+  marginTop: "6px",
+  fontSize: "13px",
+  fontWeight: 500,
+  lineHeight: 1.15,
+  color: "#666",
+};
+
+const SliderExceptPcShortCutStyle = {
+  color: "#36f",
+};
+
+const sliderInformationStyle = {
+    display : "flex",
+    flexDirection : "column",
+    alignItems : "center"
+};
+
+export {
+  SliderExceptPcMainTextStyle,
+  SliderExceptPcSubTextStyle,
+  SliderExceptPcShortCutStyle,
+  sliderInformationStyle,
+};
