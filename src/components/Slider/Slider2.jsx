210ace4c83d77aefdb87e0120f2c150970347cd5
khw970421 <khw970421@kakao.com>
2022-01-18 02:21:10 +0900

    ✨ feat : slider 버튼 이벤트 클릭 코드 추가

--- src/components/Slider/Slider2.jsx
+++ src/components/Slider/Slider2.jsx
@@ -1,44 +1,51 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "./css2.css";
 import LeftArrow from "../SliderIcon/LeftArrow";
 import RightArrow from "../SliderIcon/RightArrow";
 import { sliderDataArr } from "../../utils/constant";
-import ArrowIcon from "../HeaderIcon/ArrowIcon";
-import "./css.css";
-import {
-  sliderImageStyle,
-  sliderClickButtonStyle,
-  sliderContainerPcStyle,
-  sliderInformationPcStyle,
-  SliderPcMainTextStyle,
-  SliderPcSubTextStyle,
-  SliderPcShortCutStyle,
-  SliderPcHrStyle,
-  displayBlockStyle,
-  displayNoneStyle,
-} from "./css.js";
-const Slider2 = () => {
-  console.log(document.styleSheets[0].cssRules)
+const TOTAL_SLIDES = sliderDataArr.length - 1;
+
+export default function Slider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideRef = useRef(null);
+
+  const clickRightSliderBtn = () => {
+    if (currentSlide >= TOTAL_SLIDES) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+  const clickLeftSliderBtn = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(TOTAL_SLIDES);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  useEffect(() => {
+    slideRef.current.style.transition = "all 0.5s ease-in-out";
+    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 에니메이션을 만듭니다.
+  }, [currentSlide]);
+
   return (
-    <div className="slide">
+    <div className="Container">
       <div>
-        <button className="leftBtn">
+        <button className="leftBtn" onClick={clickLeftSliderBtn}>
           <LeftArrow />
         </button>
       </div>
-      <ul>
+      <div ref={slideRef} className="SliderContainer">
         {sliderDataArr.map(({ src, idx, mainText, subText }) => {
-          return (
-            <li key={idx}>
-              <img src={src} style={sliderImageStyle} />
-            </li>
-          );
+          return <img src={src} className="SliderImg" />;
         })}
-      </ul>
-      <button className="rightBtn">
-        <RightArrow />
-      </button>
+      </div>
+      <div>
+        <button className="rightBtn" onClick={clickRightSliderBtn}>
+          <RightArrow />
+        </button>
+      </div>
     </div>
   );
-};
-
-export default Slider2;
+}
