361e3a5d88d6a88abd124aa4b98d2990b5907913
khw970421 <khw970421@kakao.com>
2022-01-17 16:45:04 +0900

    ✨ feat : Arrow 컴포넌트 추가

---
+++ src/components/SliderIcon/LeftArrow.jsx
@@ -0,0 +1,11 @@
+const LeftArrow = () => {
+  return (
+    <span class="SvgIcon_SvgIcon__root__8vwon">
+      <svg class="SvgIcon_SvgIcon__root__svg__DKYBi" viewBox="0 0 18 18">
+        <path d="m6.045 9 5.978-5.977a.563.563 0 1 0-.796-.796L4.852 8.602a.562.562 0 0 0 0 .796l6.375 6.375a.563.563 0 0 0 .796-.796L6.045 9z"></path>
+      </svg>
+    </span>
+  );
+};
+
+export default LeftArrow;
