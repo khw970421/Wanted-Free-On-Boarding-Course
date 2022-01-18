c2e2b36637c2e7e526c63c1e926cb6ff7c4ac3d4
khw970421 <khw970421@kakao.com>
2022-01-18 16:10:20 +0900

    ✨ feat : 슬라이더 드래그 기능 구현
    draggable="false" 후 mouseDown과 mouseUp의 Y 좌표 차이 계산

--- src/components/Slider/Slider2.jsx
+++ src/components/Slider/Slider2.jsx
@@ -7,6 +7,7 @@ const TOTAL_SLIDES = sliderDataArr.length - 1;
 
 export default function Slider() {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [mouseDownPageY,setMouseDownPageY] = useState(0);
   const slideRef = useRef(null);
 
   const clickRightSliderBtn = () => {
@@ -24,6 +25,31 @@ export default function Slider() {
     }
   };
 
+  const mouseDownEvent = (e)=> {
+    setMouseDownPageY(e.pageY)
+    console.log('mouseDown',e.pageY)
+
+  }
+
+  const mouseUpEvent = (e)=> {
+    console.log('mouseUp',e.pageY ,mouseDownPageY)
+    if(e.pageY < mouseDownPageY){
+      if (currentSlide === 0) {
+        setCurrentSlide(TOTAL_SLIDES);
+      } else {
+        setCurrentSlide(currentSlide - 1);
+      }
+    }
+    else{
+      if (currentSlide >= TOTAL_SLIDES) {
+        setCurrentSlide(0);
+      } else {
+        setCurrentSlide(currentSlide + 1);
+      }
+    }
+  }
+
+
   useEffect(() => {
     slideRef.current.style.transition = "all 0.5s ease-in-out";
     slideRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 에니메이션을 만듭니다.
@@ -38,7 +64,7 @@ export default function Slider() {
       </div>
       <div ref={slideRef} className="SliderContainer">
         {sliderDataArr.map(({ src, idx, mainText, subText }) => {
-          return <img src={src} className="SliderImg" />;
+          return <img src={src} draggable="false" className="SliderImg" onMouseDown={mouseDownEvent} onMouseUp={mouseUpEvent} />;
         })}
       </div>
       <div>
