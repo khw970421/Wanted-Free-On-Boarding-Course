d8bd78b2452c1f51293fb98025e747592300e875
khw970421 <khw970421@kakao.com>
2022-01-18 18:58:13 +0900

    ✨ feat : Slider 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header/Header'
+import Slider from './components/Slider/Slider'
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Slider></Slider>
     </div>
   );
 }
